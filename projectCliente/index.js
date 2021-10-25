5ea45a11553b6514a46aafdadb92baafe77fe051
Douglas da Silva Ferreira <meliodasbrpjt1@gmail.com>
2021-10-25 20:56:23 -0300

    novos códigos

--- projectCliente/index.js
+++ projectCliente/index.js
@@ -1,21 +1,34 @@
 class Cliente {
     nome;
     cpf;
+}
+class ContaC{
     agencia;
     saldo;
 }
-const cliente1 = new Cliente();
 
+const cliente1 = new Cliente();
 cliente1.nome = "Ricardo";
 cliente1.cpf = 1122334455;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
 
 const cliente2 = new Cliente();
-
 cliente2.nome = "Lara";
 cliente2.cpf = 9922334455;
-cliente2.agencia = 1001;
-cliente2.saldo = 100;
 
+const contaCR = new ContaC();
+contaCR.saldo = 0;
+contaCR.agencia = 1001;
+console.log(contaCL.saldo);
+contaCR.saldo = 100;
+console.log(contaCL.saldo);
+let valorSacado = 200;
+contaCR.saldo -= valorSacado;
+
+const contaCL = new ContaC();
+
+contaCL.saldo = 100;
+contaCL.agencia = 1001;
+
+
+console.log(contaCL.saldo);
 console.log(cliente1,"\n",cliente2);
