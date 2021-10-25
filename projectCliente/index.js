1ad78dbbdd5450a87d21dcc5d1c0977cb1d77b00
Douglas da Silva Ferreira <meliodasbrpjt1@gmail.com>
2021-10-25 20:40:53 -0300

    atuazações do projetc Clientes

--- projectCliente/index.js
+++ projectCliente/index.js
@@ -12,9 +12,10 @@ cliente1.agencia = 1001;
 cliente1.saldo = 0;
 
 const cliente2 = new Cliente();
-cliente2.nome = "Lara"
+
+cliente2.nome = "Lara";
 cliente2.cpf = 9922334455;
 cliente2.agencia = 1001;
 cliente2.saldo = 100;
 
-console.log(cliente1);
+console.log(cliente1,"\n",cliente2);
