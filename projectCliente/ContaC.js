9d508c2a225e66ddc99dbe76c48ca18fb77d8d06
Douglas da Silva Ferreira <meliodasbrpjt1@gmail.com>
2021-10-26 10:45:49 -0300

    adicionando código parte 2/6

---
+++ projectCliente/ContaC.js
@@ -0,0 +1,24 @@
+export class ContaC {
+    agencia;
+    cliente;
+    _saldo = 0;
+    //parâmetros
+    sacar(valor) {
+        if (this._saldo >= valor) {
+            this._saldo -= valor;
+            return valor;
+        }
+    }
+
+    depositar(valor) {
+        if (valor <= 0) {
+            return valor;
+        }
+        this._saldo += valor;
+    }
+
+    transferir(valor, conta){
+        const valorSacado = this.sacar(valor);
+        conta.depositar(valorSacado);
+    }
+}
--- projectCliente/index.js
+++ projectCliente/index.js
@@ -1,25 +1,5 @@
-//métodos
-class Cliente {
-    nome;
-    cpf;
-}
-class ContaC{
-    agencia;
-    _saldo = 0;
-    //parâmetros
-    sacar(valor){
-        if(this._saldo >= valor){
-            this._saldo -= valor;
-            return valor;
-        }
-    }
-    depositar(valor){
-        if(valor <= 0){
-            return valor;
-        }
-        this._saldo += valor;
-    }
-}
+import { Cliente } from "./Cliente.js";
+import { ContaC } from "./ContaC.js";
 //dados
 const cliente1 = new Cliente();
 cliente1.nome = "Ricardo";
@@ -32,12 +12,18 @@ cliente2.cpf = 9922334455;
 //valor
 const contaCR = new ContaC();
 contaCR.agencia = 1001;
+contaCR.cliente = cliente1;
+contaCR.depositar(500);
 
+const contaCL = new ContaC();
+contaCL.cliente = cliente2;
+contaCL.agencia = 102;
+contaCR.transferir(200, contaCL)
 //opereções 
-contaCR.depositar(100);
-contaCR.sacar(50);
+
+
 
 
 //final
 console.log(contaCR);
-console.log(cliente1,"\n",cliente2);
+console.log(contaCL);
