38e5051fc22667608832bd91ec481b7505b82e8a
Douglas da Silva Ferreira <meliodasbrpjt1@gmail.com>
2021-10-28 22:36:31 -0300

    atulizando código

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -1,2 +1,15 @@
 var titulo = document.querySelector(".titulo");
-titulo.textContent = "Banana";
+titulo.textContent = "Aparecida Nutricionista";
+
+var paciente = document.querySelector("#primeiro-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+var tdImc = paciente.querySelector(".info-imc");
+var imc = peso / (altura * altura);
+
+tdImc.textContent = imc;
