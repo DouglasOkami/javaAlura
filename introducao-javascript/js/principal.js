e5ecb3e6e1616f01c219fe55195c3c4d63f67b8a
Douglas da Silva Ferreira <meliodasbrpjt1@gmail.com>
2021-10-31 21:51:01 -0300

    códigos maravilhosos feitos

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -1,15 +1,36 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
-var paciente = document.querySelector("#primeiro-paciente");
+var pacientes = document.querySelectorAll(".paciente");
 
-var tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textContent;
+for (let i = 0; i < pacientes.length; i++) {
+    var paciente = pacientes[i];
 
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
 
-var tdImc = paciente.querySelector(".info-imc");
-var imc = peso / (altura * altura);
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
 
-tdImc.textContent = imc;
+    var tdImc = paciente.querySelector(".info-imc");
+
+    var pesoValido = true;
+    var alturaValida = true;
+
+    if (peso <= 0 || peso >= 250) {
+        console.log("Peso inválido!")
+        pesoValido = false;
+        tdImc.textContent = "Peso inválido";
+    }
+    if (altura <= 0 || altura >= 3.00) {
+        console.log("Altura inválido!")
+        alturaValida = false;
+        tdImc.textContent = "Altura inválida"
+    }
+    if (pesoValido = true && alturaValida) {
+        var imc = peso / (altura * altura);
+        tdImc.textContent = imc.toFixed(2);
+    }
+}
+
+
