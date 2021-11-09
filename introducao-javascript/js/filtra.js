4148154a6bcc5cc840263991a3fef5b055f464ea
Douglas da Silva Ferreira <meliodasbrpjt1@gmail.com>
2021-11-09 08:43:56 -0300

    filtro de pacientes quase finalizado

--- introducao-javascript/js/filtra.js
+++ introducao-javascript/js/filtra.js
@@ -1,11 +1,24 @@
-var campoFiltro = document.querySelectorAll("#filtrar-tabela");
+var campoFiltro = document.querySelector("#filtrar-tabela");
 
-campoFiltro.addEventListener("input",function(){
+campoFiltro.addEventListener("input", function () {
     console.log(this.value);
-    var pacientes = document.querySelector(".paciente");
-    for (var i = 0; i < pacientes.length; i++) {
-        var paciente = pacientes [i];
-        var TdNome = paciente.querySelector(".info-nome");
-        var nome = paciente.textContent;
+    var pacientes = document.querySelectorAll(".paciente");
+
+    if (this.value.length > 0) {
+        for (var i = 0; i < pacientes.length; i++) {
+            var paciente = pacientes[i]
+            var tdNome = paciente.querySelector(".info-nome");
+            var nome = tdNome.textContent;
+            if (nome != this.value) {
+                paciente.classList.add("invisivel");
+            } else {
+                paciente.classList.remove("invisivel");
+            }
+        }
+    } else {
+        for (let i = 0; i < pacientes.length; i++) {
+            var paciente = pacientes[i];
+            paciente.classList.remove("invisivel");
+        }
     }
 });
